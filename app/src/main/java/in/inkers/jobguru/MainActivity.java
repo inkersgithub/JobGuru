463e8b2377bf7b5fad6be77cd97a3832f122f367
Zade <mubaraknet123789@gmail.com>
2017-08-24 01:21:38 +0530

    basic model

--- app/src/main/java/in/inkers/jobguru/MainActivity.java
+++ app/src/main/java/in/inkers/jobguru/MainActivity.java
@@ -1,13 +1,35 @@
 package in.inkers.jobguru;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private WebView wv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        wv1=findViewById(R.id.webView);
+        wv1.setWebViewClient(new MyBrowser());
+        String url = "http://jiss.in/jobguru";
+        wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv1.loadUrl(url);
+
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
